82564c8b1fe78704e68c144eff590fdf524a6fc3
zubiyabintamostofa <zubiyabinte123@gmail.com>
2022-03-08 12:04:09 +0600

    one

---
+++ alert.js
@@ -0,0 +1,18 @@
+ console.log('i am here')
+ //alert('Maa is comming!!')
+ const maComing=()=>{
+     alert('Ammu is comming open the book')
+ }
+ const askPicnic=()=>{
+    const response= confirm('R u going to picnic?')
+    console.log(response);
+    if(response===true){
+        alert('amk fee te bkash kore de');
+    }
+    else{
+        console.log('dure gea mor')
+    }
+ }
+ const askName=()=>{
+     prompt('what is ur name??')
+ }
